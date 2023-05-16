45a3c0885fcd961fbe8eaa170867cb912ea1bd94
Clinton Brito <clintoncsb@gmail.com>
2023-05-15 21:40:23 -0300

    :sparkles: [Req. 2] Header Component -- ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-class Header extends Component {
+class Header extends React.Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <h1>💱 TrybeWallet 💳️</h1>
+        <div>
+          <h2 data-testid="email-field">{ email }</h2>
+          <h2 data-testid="total-field">{0}</h2>
+          <h2 data-testid="header-currency-field">BRL</h2>
+        </div>
+      </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <h1>TrybeWallett</h1>;
+    return (
+      <>
+        <Header />
+        <h2>teste</h2>
+      </>
+    );
   }
 }
 
